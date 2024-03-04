caf550f664c995d8c0c3467e009be59b0e23fbdb
unknown <mohsen.bashirzadeh@gmail.com>
2024-03-04 21:40:12 +0330

    add req params and req.query

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ const app = express();
 
 //get post put delete
 
+//req.param   -- > https://localhost:8080/products:productId
+//req.body    
+//req.query   --> https://localhost:8080/search?key=great
+
 app.use((req,res,next)=>{
     console.log(`Current time: ${new Date().toLocaleString()}`);
     next();
+})
+
+app.get('/search',(req,res)=>{
+    const {key} = req.query;
+    console.log(req.query);
+    res.send("ok");
 })
 
 
@@ -14,19 +24,28 @@ app.get('/contact',(req,res)=>{
 })
 
 const products = [
-    {name:"shirt"},
-    {name:"shoes"},
-    {name:"scarfs"}
+    {id:1,name:"shirt"},
+    {id:2,name:"shoes"},
+    {id:3,name:"scarfs"}
 ];
 
+app.get('/products/:productId',(req,res)=>{
+    const {productId} = req.params;
+    let product = products.find(item=>item.id === +productId);
+    console.log("the result product is : ", product);
+    res.send(product.name);
+    })
+
 app.get('/products',(req,res)=>{
-    // res.status(200).json("ok");
+    // res.status(200).send("ok");
     res.json({
         status: 'ok',
         statusCode: 200,
         data: products,
     })
 })
+
+
 
 app.get('/',(req,res)=>{
     res.send("Home");
